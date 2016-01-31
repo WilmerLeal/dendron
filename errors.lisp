20dcf28b1c8531f5805a62c2f23ad10bf509295f
WilmerLeal <wilmerlealj@gmail.com>
2016-01-30 19:00:59 -0500

    Create errors.lisp

---
+++ errors.lisp
@@ -0,0 +1,19 @@
+;-*-Lisp-*-
+
+;; Error conditions in dendron
+(in-package :dendron)
+	 
+;; (define-condition input-error ()
+;;   )
+
+;; (define-condition no-reading-permission (input-error)
+;;   )
+
+;; (define-condition input-file-does-not-exist (input-error)
+;; )
+
+;; (define-condition unknown-input-format (input-error)
+;; )
+
+;; (define-condition empty-leaf (input-error)
+;; )
